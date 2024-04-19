f927d603f264146c1816971767ae85e147435013
unknown <alch3mist94@protonmail.com>
2024-04-19 15:10:01 +0200

    fixed fuse string constructor

--- tests/fuse_test.py
+++ tests/fuse_test.py
@@ -12,7 +12,7 @@ nn.sequential('Number Detector',
        topology=[5, 3, 3, 1],
        activation_overlay=['linear', 'relu', 'relu', 'relu', 'linear'])
 nn.initialize('uniform',
-              weight_range=[-.1, .2],
+              weight_range=[-.2, .2],
               bias_range=[-.2, .2])
 
 fuse(nn, np.float32)
